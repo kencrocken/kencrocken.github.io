b95053d6772eb5940f7223f19c57df1e596fbb67
Ken Crocken <kcrocken@gmail.com>
2015-01-11 22:39:47 -0500

    add the parts back in

--- scripts/app.js
+++ scripts/app.js
@@ -1,16 +1,15 @@
 var mySite = angular.module('mySite', ['ngResource']);
 
-mySite
-.controller('contactCtrl', function($scope, $http, $resource) {
+mySite.controller('contactCtrl', function($scope, $http, $resource) {
     $http.defaults.useXDomain = true;
     // $http.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
     $scope.message = {};
     $scope.submitForm = function(isValid, message) {
         $scope.submitted = true;
-        console.log(message);
+        // console.log(message);
         if (isValid) {
-            console.log('valid form');
-            console.log($scope.message);
+            // console.log('valid form');
+            // console.log($scope.message);
             $http.post( '//formspree.io/kcrocken@gmail.com', $scope.message, {
                 params:  $scope.message,
                 headers: {
@@ -35,44 +34,19 @@ mySite
                     return str.join("&");
                 }
             }).
-              success(function(data, status, headers, config) {
-                        console.log('status');
-                        $scope.status = status;
-                        $scope.data = data;
-                        $scope.headers = headers;
-                        $scope.config = config;
-                        console.log (data,status,headers,config);
-                  }).
-                  error(function(data, status, headers, config) {
+                success(function(data, status, headers, config) {
+                        console.log('Success!');
+                }).
+                    error(function(data, status, headers, config) {
                         console.log('error');
                         $scope.status = status;
                         $scope.data = data;
                         $scope.headers = headers;
                         $scope.config = config;
                         console.log (data,status,headers,config);
-                  });
-            //     url     : ,
-            //     method  : 'POST',
-            //     data    : $.param($scope.message),
-            //     headers : {
-            //         // 'Access-Control-Allow-Origin': 'http://localhost:3000', 
-            //         'Content-Type': 'application/x-www-form-urlencoded' 
-            //         },  // set the headers so angular passing info as form data (not request payload)
-            //     dataType: "jsonp"
-            //     // withCredentials: true
-            // })
-        //     .$promise.then(function(data) {
-        //         console.log(data);
+                });
 
-        //         if (!data.success) {
-        //           // if not successful, bind errors to error variables
-        //           $scope.errorName = data.errors;
-        //         } else {
-        //           // if successful, bind success message to message
-        //           $scope.message = data.message;
-        //         }
-        //     });
-        $scope.submittedValid = true;          
+            $scope.submittedValid = true;          
         } else {
             console.log('not valid');
         }
