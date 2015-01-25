72e220e527574e0185ea78138e4cfc6154c79fa4
Ken Crocken <kcrocken@gmail.com>
2015-01-25 03:03:07 -0500

    code cleanup, remove console

--- scripts/controllers/treehouseCtrl.js
+++ scripts/controllers/treehouseCtrl.js
@@ -1,7 +1,6 @@
 mySite.controller('treehouseCtrl', function($scope, TreehouseProfileService){
     TreehouseProfileService.badges().then(function(badges){
         $scope.badges = badges;
-        console.log('from controller:  ' + $scope.badges);
     }); 
 });
     
--- scripts/services.js
+++ scripts/services.js
@@ -1,29 +1,23 @@
 mySite.service('TreehouseProfileService', function($http, $q) {
     this.badges = function(name) {
         var d = $q.defer();
-
         $http({
             method: 'GET',
             url: 'http://teamtreehouse.com/kennethcrocken.json'
         }).
             then(function(response){
-
-                var badges = _.map(response.data.badges, function(badge) {
-                                    
+                var badges = _.map(response.data.badges, function(badge) {    
                         return {
                             name: badge['name'],
                             icon: badge['icon_url'],
                             date: badge['earned_date']
                         };
                     })
-
-                console.log('from servics: ' + badges);
                 d.resolve(badges);
-                },
-                     function(error) {
+                }, function(error) {
                         d.reject(error);
                     }
-                );
+            );
         return d.promise;
     }
 });
