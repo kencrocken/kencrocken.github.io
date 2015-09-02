48cf7993abe12d7391f55233161358d8a28428e5
Ken Crocken <kcrocken@gmail.com>
2015-09-02 11:19:23 -0400

    Lots of changes, adds lunar phase project

--- gulpfile.js
+++ gulpfile.js
@@ -49,10 +49,7 @@ gulp.task('sass', function () {
         .pipe(autoprefixer({
             browsers: [
               'last 2 versions',
-              'safari 5',
-              'ie 8',
               'ie 9',
-              'opera 12.1',
               'ios 6',
               'android 4'
             ],
