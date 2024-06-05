cd21f593fb370a80fe8589d33515657d14d85803
Ken Crocken <kcrocken@gmail.com>
2024-06-05 13:49:43 -0400

    feat: Set base URL to "new-site" in Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
+  base: "new-site",
   integrations: [icon({
     include: {
       "fa6-brands": ["linkedin", "github"],
