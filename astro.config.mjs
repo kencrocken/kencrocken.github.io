31d6b8a67325e42778aeb5136032d1c813814392
Ken Crocken <kcrocken@gmail.com>
2024-09-24 03:01:11 -0400

    refactor: Update ShopTalk component to use TypeScript for better type safety

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://kencrocken.github.io',
-  integrations: [icon({
-    include: {
-      "fa6-brands": ["linkedin", "github"],
-      "fa6-solid": ["moon"],
-    }
-  })]
+  site: "https://kencrocken.github.io",
+  integrations: [
+    icon({
+      include: {
+        "fa6-brands": ["linkedin", "github"],
+        "fa6-solid": ["moon", "gears"],
+      },
+    }),
+  ],
 });
